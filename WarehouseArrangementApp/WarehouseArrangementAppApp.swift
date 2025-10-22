31de313c72b9d58cb7d4faed234ef175f90f1816
n salehi <nimasalehi.auth@icloud.com>
2025-10-22 11:56:15 +0330

    Initial Commit

---
+++ WarehouseArrangementApp/WarehouseArrangementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarehouseArrangementAppApp.swift
+//  WarehouseArrangementApp
+//
+//  Created by Nima Salehi on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct WarehouseArrangementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WarehouseArrangementAppUITests/WarehouseArrangementAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WarehouseArrangementAppUITestsLaunchTests.swift
+//  WarehouseArrangementAppUITests
+//
+//  Created by Nima Salehi on 10/22/25.
+//
+
+import XCTest
+
+final class WarehouseArrangementAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
